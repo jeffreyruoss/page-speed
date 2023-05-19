1eed4a465b874d0889e07d9ef87a01052d285af1
Jeff Ruoss <jeffreyruoss@gmail.com>
2023-05-19 13:16:52 -0700

    Clean up.

--- index.js
+++ index.js
@@ -2,57 +2,61 @@ require('dotenv').config();
 const fetch = require('cross-fetch');
 const chalk = require('chalk');
 
-if (!process.argv[2]) {
+const URL_ARG_INDEX = 2;
+const MINUTE = 60000;
+const DEFAULT_STRATEGY = 'mobile';
+
+if (!process.argv[URL_ARG_INDEX]) {
   console.log(chalk.yellow('Please provide a URL to test'));
   console.log(chalk.yellow('Example: npm start -- mysite.com'));
   process.exit(1);
 }
 
-const url = 'https://' + encodeURIComponent(process.argv[2]);
+const url = `https://${encodeURIComponent(process.argv[URL_ARG_INDEX])}`;
 const apiKey = process.env.API_KEY;
 let firstRun = true;
 
-async function checkPageSpeed(strategy) {
-  const apiEndpoint = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${url}&key=${apiKey}&strategy=${strategy}`;
-
-  if (firstRun) {
-    console.log(chalk.gray(`Running ${strategy.toUpperCase()} Page Speed Test on:`)); 
-    console.log(chalk.blue(url));
-    console.log();
-    console.log(chalk.gray('API Endpoint:')); 
-    console.log(chalk.blue(apiEndpoint));
-    console.log();
-    console.log(chalk.gray('Checking every')); 
-    console.log(chalk.blue(`${freqency / oneMinute} minutes`));
-    console.log();
-    
-    firstRun = false;
-  }
-
+async function fetchFromEndpoint(apiEndpoint) {
   try {
     const response = await fetch(apiEndpoint);
     const data = await response.json();
 
-    console.log(`${strategy.toUpperCase()} Page Speed Score: `, data.lighthouseResult.categories.performance.score * 100);
-
-    // You can now store this score and timestamp in a database to keep track over time
+    return data;
   } catch (error) {
-    console.log(error);
+    console.error(chalk.red('Failed to fetch data from endpoint:', apiEndpoint));
+    throw error;
   }
 }
 
-// Schedule the function to run daily or however frequently you want
-const oneMinute = 60000;
-const oneHour = oneMinute * 60;
-const oneDay = oneHour * 24;
-const freqency = oneMinute;
+function displayFirstRunDetails(strategy, apiEndpoint, frequencyInMinutes) {
+  console.log(chalk.gray(`Running ${strategy.toUpperCase()} Page Speed Test on:`)); 
+  console.log(chalk.blue(url));
+  console.log();
+  console.log(chalk.gray('API Endpoint:')); 
+  console.log(chalk.blue(apiEndpoint));
+  console.log();
+  console.log(chalk.gray('Checking every')); 
+  console.log(chalk.blue(`${frequencyInMinutes} minutes`));
+  console.log();
+}
+
+async function checkPageSpeed(strategy = DEFAULT_STRATEGY) {
+  const apiEndpoint = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${url}&key=${apiKey}&strategy=${strategy}`;
+
+  if (firstRun) {
+    const frequencyInMinutes = MINUTE / MINUTE;
+    displayFirstRunDetails(strategy, apiEndpoint, frequencyInMinutes);
+    firstRun = false;
+  }
+
+  const data = await fetchFromEndpoint(apiEndpoint);
+
+  console.log(`${strategy.toUpperCase()} Page Speed Score: `, data.lighthouseResult.categories.performance.score * 100);
+  // You can now store this score and timestamp in a database to keep track over time
+}
 
 // First run
-checkPageSpeed('mobile', freqency);
-// checkPageSpeed('desktop', freqency);
+checkPageSpeed();
 
 // Run periodically
-setInterval(() => {
-  checkPageSpeed('mobile', freqency);
-  // checkPageSpeed('desktop', freqency);
-}, freqency);
+setInterval(checkPageSpeed, MINUTE);
